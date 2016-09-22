80a4bfe6001ee9f1909d58b3f3c6cb85baf31b21
James Santos <james.santos-calacat@gettyimages.com>
2016-09-22 12:04:01 -0700

    Add Getty Images API BDD Scenarios from https://github.com/gettyimages/gettyimages-api_bdd

---
+++ gettyimagesapi-sdk/src/test/java/com/gettyimages/AuthenticationContext.java
@@ -0,0 +1,67 @@
+package com.gettyimages;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by jsantos on 9/22/16.
+ */
+public class AuthenticationContext {
+
+    @Given("^I have an apikey$")
+    public void i_have_an_apikey() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^an apisecret$")
+    public void an_apisecret() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^a username$")
+    public void a_username() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^a password$")
+    public void a_password() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I ask the sdk for an authentication token$")
+    public void i_ask_the_sdk_for_an_authentication_token() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^a token is returned$")
+    public void a_token_is_returned() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^a refresh token$")
+    public void a_refresh_token() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I request an access token$")
+    public void i_request_an_access_token() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^an access token is returned$")
+    public void an_access_token_is_returned() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
--- gettyimagesapi-sdk/src/test/java/com/gettyimages/RunBddScenariosTest.java
+++ gettyimagesapi-sdk/src/test/java/com/gettyimages/RunBddScenariosTest.java
@@ -1,9 +1,8 @@
 package com.gettyimages;
 
-import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-public class RunFakeTest {
+public class RunBddScenariosTest {
 }
