a53c7851545ef38e6adf0405a89e4441c3d8d33b
James Santos <james.santos-calacat@gettyimages.com>
2016-09-25 20:42:41 -0700

    Update cucumber tests to only target authentication.feature for now

--- gettyimagesapi-sdk/src/test/java/com/gettyimages/RunBddScenariosTest.java
+++ gettyimagesapi-sdk/src/test/java/com/gettyimages/RunBddScenariosTest.java
@@ -1,8 +1,10 @@
 package com.gettyimages;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/com/gettyimages/bdd-scenarios/authentication.feature") // TODO remove this once all feature files have been implemented
 public class RunBddScenariosTest {
 }
